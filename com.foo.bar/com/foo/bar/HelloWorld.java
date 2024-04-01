b4ecb7f29024999e7fc99668fefa25e8c1b50507
Gautham Krishnan <140151984+gauthamkrishnanibm@users.noreply.github.com>
2024-04-01 11:47:27 +0530

    Update HelloWorld.java

--- com.foo.bar/com/foo/bar/HelloWorld.java
+++ com.foo.bar/com/foo/bar/HelloWorld.java
@@ -3,6 +3,7 @@ package com.foo.bar;
 import java.util.logging.Logger;
 
 public class HelloWorld {
+  private static final Logger LOG = Logger.getLogger(HelloWorld.class.getName());
   public static void main(String[] args) {
     LOG.info("Hello World!");
   }
